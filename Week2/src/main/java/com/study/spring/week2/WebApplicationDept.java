e23aa09c33f882c9075de3afbc0150d96b8c790f
U Park <upark@Uui-MacBook-Pro.local>
2019-03-10 14:17:19 +0900

    upload week2

---
+++ Week2/src/main/java/com/study/spring/week2/WebApplicationDept.java
@@ -0,0 +1,9 @@
+package com.study.spring.week2;
+
+public class WebApplicationDept extends Department {
+
+	public void printDepartmentInfo() {
+		System.out.println("HumanResourceDept : printDepartmentInfo()");
+	}
+	
+}
